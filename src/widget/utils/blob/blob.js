ba945bf698b57fb09c703eb89de80c0eb11187b0
lolasilvag <lolasilva.25@gmail.com>
2025-11-29 17:19:06 -0300

    .

--- src/widget/utils/blob/blob.js
+++
@@ -1,9 +0,0 @@
-import utils from "./utils.js"
-import { G } from "@svgdotjs/svg.js"
-
-export default function (key) {
-    let path = utils.getPath(key).path
-    let g = new G()
-    g.path(path)
-    return g
-}
